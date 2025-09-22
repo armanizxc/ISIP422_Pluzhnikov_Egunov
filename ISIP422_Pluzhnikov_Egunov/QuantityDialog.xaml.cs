f82b1d36edec69114f3206bdc71d613c004bbbf4
armanizxc <al.pl6@mail.ru>
2025-09-22 14:47:43 +0300

    QuantityDialog

---
+++ ISIP422_Pluzhnikov_Egunov/QuantityDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace ISIP422_Pluzhnikov_Egunov
+{
+    public partial class QuantityDialog : Window
+    {
+        public int Quantity { get; private set; }
+
+        public QuantityDialog(string title, string message, int maxQuantity = int.MaxValue)
+        {
+            InitializeComponent();
+            Title = title;
+            MessageText.Text = message;
+        }
+
+        private void OKButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (!int.TryParse(QuantityTextBox.Text, out int quantity) || quantity <= 0)
+            {
+                MessageBox.Show("Введите корректное количество!", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Quantity = quantity;
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
